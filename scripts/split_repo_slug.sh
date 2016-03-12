a2c2284441573b6f5424a9c2469f8b7503e4e98f
reno- <contact@rubiano.fr>
2016-03-12 14:58:15 +0100

    reduce verbosity

--- scripts/split_repo_slug.sh
+++ scripts/split_repo_slug.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-set -v
-
-if [[ ${TRAVIS_REPO_SLUG} =~ ([^,]+).*"/"([^,]+) ]]; then 
+if [[ ${TRAVIS_REPO_SLUG} =~ ([^,]+).*"/"([^,]+) ]]; then
     OWNER=${BASH_REMATCH[1]}
     REPO=${BASH_REMATCH[2]}
 fi
