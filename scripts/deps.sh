92217d9dc6b91803272d25ea2e25ab57f37c5598
reno- <contact@rubiano.fr>
2017-10-21 12:03:19 +0200

    wip

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -6,17 +6,16 @@ case "$TRAVIS_OS_NAME" in
   linux)
     echo "START LINUX"
     # install OSC dependancy
-    sudo apt-get -y install liblo7
     echo "END LINUX"
   ;;
   osx)
     echo "START OSX"
     # install OSC dependancy
-    brew install liblo
     echo "END OSX"
   ;;
 esac
 
 sudo pip3 install Cython
-sudo pip3 install pyliblo
 sudo pip3 install pyinstaller
+sudo pip3 install PyQt5
+
